8c65f57a38c7c207091771665f1792f1c370de1e
umxie <developerumxie@gmail.com>
2026-01-15 12:25:01 +0530

    frist code

---
+++ function.js
@@ -0,0 +1,7 @@
+// function sajid (a,b){
+//  console.log(a+b);
+    
+// }
+// sajid(100,50)
+// sajid("fggg",1234567890)
+ 
---
+++ scrjipt.js
@@ -0,0 +1,29 @@
+var a = 10
+
+var a = "jhedkjfhkj"
+a = 9999
+// console.log("this is a : ",a);
+
+
+
+// let 
+
+let b = 300
+// const b = 6000 redeclarion is not possible
+b = 30000
+
+// console.log(b);
+
+
+
+const c = 3.14
+// const c = 888
+// c = 5555
+// console.log(c);
+
+
+
+console.log("hellow owrld");
+
+
+
