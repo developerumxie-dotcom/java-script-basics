b97de4e137fda285cd708cd108e722d7f5d20994
umxie <developerumxie@gmail.com>
2026-01-22 13:22:43 +0530

    updated

---
+++ array/array.js
@@ -0,0 +1,33 @@
+// const a = 10
+// const b = "sajid"
+// const c = "umair"
+
+
+// let person = ["sajid","umair","himran"]
+// let age = [10,7,9]
+// for (let i=0;i=1;i++) {
+
+    
+//     console.log(person[0],age[2]);
+// }
+
+// console.log(person[0]);   //arrayName[indexNumber]
+// person.pop()
+// person.push("imran")
+// person.shift()
+// person.unshift("sajid")
+// console.log(person[0]);
+
+
+let students = [
+    {name:"sajid",age:25},
+    {name:"umair",age:25},
+    {name:"himran",age:25}
+]
+
+for(let i=0;i<=2;i++){  
+    console.log(students[i].name);  
+}
+
+
+
---
+++ array/arraymethods.js
@@ -0,0 +1,22 @@
+//map, forEach, filter, find
+
+// 1.map
+
+// let person = ["sajid","umair","himran"]
+
+// person.map((data,index,array)=>{
+//     console.log(data);
+    
+// })
+
+
+// const umair = (callback) => {
+//     callback()
+// }
+
+// umair(() => {
+//     console.log("dslkfjdkufjhk");
+    
+// }
+
+//2.forEach
