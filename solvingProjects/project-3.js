2bf31bb1963af3996871e50007382be7c4d71b0d
umxie <developerumxie@gmail.com>
2026-01-27 13:19:12 +0530

    projct 3jd ubdated

---
+++ solvingProjects/project-3.js
@@ -0,0 +1,16 @@
+const details = [{ name: "umair", age: 21 },
+{ name: "sajid", age: 99 },
+{ name: "imran", age: 9 }]
+
+let mapFunction = details.map((data) =>{
+    return data.name
+    
+    
+    
+
+})
+
+console.log(mapFunction);
+
+
+
