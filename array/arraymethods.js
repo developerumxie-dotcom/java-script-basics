02524aacd592978eaceaad6abd01e1bcb4915792
umxie <developerumxie@gmail.com>
2026-01-27 13:17:13 +0530

    ubdated

--- array/arraymethods.js
+++ array/arraymethods.js
@@ -1,13 +1,40 @@
-//map, forEach, filter, find
+// map, forEach, filter, find
 
 // 1.map
 
 // let person = ["sajid","umair","himran"]
+// let num = [1,2,4,5]
 
-// person.map((data,index,array)=>{
-//     console.log(data);
+
+// let mapFunction = num.map((data,index,array)=>{
+//     // console.log(data*2);
+//     return array
     
 // })
+
+// console.log(mapFunction);  
+
+
+//forEach ....................................................................
+
+
+// let num = [1,2,4,5]
+
+// let forEachFucntion = num.forEach((item,sjaid)=>{     //forEach(function0)
+//     // console.log(item*3);
+//     return sjaid
+    
+// })
+
+// console.log(forEachFucntion);
+
+
+
+
+
+
+
+
 
 
 // const umair = (callback) => {
@@ -17,6 +44,33 @@
 // umair(() => {
 //     console.log("dslkfjdkufjhk");
     
-// }
+// })
 
-//2.forEach
+
+
+
+// filter............................................
+
+const age = [22,34,55,77]
+
+var a = age.filter((data)=>{
+    return data 
+})
+
+console.log("this is a filter :",a);
+
+
+
+
+//find
+
+const ageeee = [22,34,55,77]
+
+const finddd = age.find((data)=>{
+    return data 
+})
+
+console.log("this is a find : ",finddd);
+
+
+
